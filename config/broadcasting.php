8e652f497c8e5793486062ac374041e64a0cab14
bernardk23 <bernardkissi@live.com>
2020-06-04 20:26:24 +0000

    modified the websocket and broadcasting configuration

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,7 +39,7 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 //'useTLS' => true,
                 'encrypted' => true,
-                'host' => 'api.ghcovid19cases.live',
+                'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'https',
                 'curl_options' => [
